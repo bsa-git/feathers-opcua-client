82c8b08c6726b819023f4b940ae9bb424fbe47fe
Sergey Beskorovainy <bs261257@gmail.com>
2024-10-13 11:20:37 +0300

    Update

--- src/components/layout/LeftDrawer.vue
+++ src/components/layout/LeftDrawer.vue
@@ -1,19 +1,33 @@
 <template>
-  <v-navigation-drawer v-model="compDrawer" :dark="$vuetify.theme.dark" :width="320" absolute temporary>
+  <v-navigation-drawer
+    v-model="compDrawer"
+    :dark="$vuetify.theme.dark"
+    :width="320"
+    absolute
+    temporary
+  >
     <v-list-item>
       <v-list-item-avatar>
         <v-img :src="userAvatar()"></v-img>
       </v-list-item-avatar>
 
       <v-list-item-content>
-        <v-list-item-title>{{ $t('common.hi') }} {{ user ? user.firstName : $t('common.guest') }}</v-list-item-title>
+        <v-list-item-title
+          >{{ $t('common.hi') }}
+          {{ user ? user.firstName : $t('common.guest') }}</v-list-item-title
+        >
       </v-list-item-content>
     </v-list-item>
 
     <v-divider></v-divider>
 
     <v-list dense>
-      <v-list-item v-for="menuItem in menuItems" :key="menuItem.title" :to="menuItem.path" link>
+      <v-list-item
+        v-for="menuItem in menuItems"
+        :key="menuItem.title"
+        :to="menuItem.path"
+        link
+      >
         <v-list-item-icon>
           <v-icon>{{ menuItem.icon }}</v-icon>
         </v-list-item-icon>
@@ -39,20 +53,19 @@ export default {
     drawer: Boolean,
     menuItems: {
       type: Array,
-      default: function () {
+      default: function() {
         return [{ title: 'Home', icon: 'mdi-home', path: '/' }]
       }
     }
   },
 
   setup(props, context) {
-
     const { $store, $validator, $vuetify, $i18n, $router } = context.root
     // const mini = ref(true)
 
     // Computed state
     const user = computed(() => $store.state['auth']['user'])
-    if(isDebug && user.value) debug('setup.user:', user.value)
+    if (isDebug && user.value) debug('setup.user:', user.value)
     // Computed getters
     const config = computed(() => $store.getters.getConfig)
     const compDrawer = computed({
@@ -67,11 +80,10 @@ export default {
     const userAvatar = () => {
       // const avatar = user.value? user.value.avatar : ''
       // return avatar;
-      const email = user.value? user.value.email : ''
-      const avatar = new Avatar(email);
-      if(isDebug && avatar) debug('setup.user.avatar:', avatar)
-      return avatar.imageUrl();
-
+      const email = user.value ? user.value.email : ''
+      const avatar = new Avatar(email)
+      if (isDebug && avatar) debug('setup.user.avatar:', avatar)
+      return avatar.imageUrl()
     }
 
     return {
